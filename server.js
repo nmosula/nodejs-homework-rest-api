4638567ea81f09e0a72e616cd16f4c1f5d435444
Nazar Mosula <nmosula@gmail.com>
2023-05-27 16:51:24 +0300

    added mongoDB connection

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoos = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const DB_HOST = "mongodb+srv://nmosula:bnjfdjsVtbreXLg4@cluster0.lrcmpqm.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoos.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
