90fdbda919d00a854dc359259655f75ddb4fd59e
Nazar Mosula <nmosula@gmail.com>
2023-05-31 20:36:55 +0300

    Added pagination & find by owner

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,7 +1,11 @@
 const { Contact } = require("../../models/contact");
 
 const getAllContacts = async (req, res) => {
-    const result = await Contact.find({}, "-createdAt -updatedAt");
+    const { _id: owner } = req.user;
+    const { page = 1, limit = 20 } = req.query;
+    const skip = (page - 1) * limit;
+    
+    const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "_id email subscription");
     res.json(result)
 }
 
