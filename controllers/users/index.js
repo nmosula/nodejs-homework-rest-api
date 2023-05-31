1e39f22f069ba5ee7ef010dd02fd4018244cf5b4
Nazar Mosula <nmosula@gmail.com>
2023-05-31 20:34:26 +0300

    created getCurrent Controllers

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -3,9 +3,11 @@ const { ctrlWrapper } = require("../../utils");
 const register = require("./register");
 const login = require("./login");
 const logout = require("./logout");
+const getCurrent = require("./getCurrent");
 
 module.exports = {
     register: ctrlWrapper(register),
     login: ctrlWrapper(login),
     logout: ctrlWrapper(logout),
+    getCurrent: ctrlWrapper(getCurrent),
 }
